38cd857404255f7e7a91e2889e7b1ef8450afe2b
wynn5a <winminy@163.com>
2022-06-27 17:41:00 +0800

    task: refactor to instance type

--- project-2/src/main/java/io/github/wynn5a/di/ContainerConfig.java
+++ project-2/src/main/java/io/github/wynn5a/di/ContainerConfig.java
@@ -11,15 +11,14 @@ import java.util.function.Supplier;
 
 public class ContainerConfig {
 
-  private final Map<Class<?>, InstanceSupplier<?>> suppliers = new HashMap<>();
   private final Map<InstanceType, InstanceSupplier<?>> instanceSuppliers = new HashMap<>();
 
-  record InstanceType(Class<?> type, Annotation qualifier) {
+  public record InstanceType(Class<?> type, Annotation qualifier) {
 
   }
 
   public <T> void bind(Class<T> type, T instance) {
-    suppliers.put(type, c -> instance);
+    instanceSuppliers.put(new InstanceType(type, null), c -> instance);
   }
 
   public <T> void bind(Class<T> type, T instance, Annotation... qualifiers) {
@@ -29,7 +28,7 @@ public class ContainerConfig {
   }
 
   public <T, I extends T> void bind(Class<T> type, Class<I> instanceType) {
-    suppliers.put(type, new InjectedInstanceSupplier<>(instanceType));
+    instanceSuppliers.put(new InstanceType(type, null), new InjectedInstanceSupplier<>(instanceType));
   }
 
   public <T, I extends T> void bind(Class<T> type, Class<I> instanceType, Annotation... qualifiers) {
@@ -40,7 +39,7 @@ public class ContainerConfig {
 
 
   public Container getContainer() {
-    suppliers.keySet().forEach(c -> checkDependencies(c, new Stack<>()));
+    instanceSuppliers.keySet().forEach(c -> checkDependencies(c, new Stack<>()));
 
     return new Container() {
 
@@ -51,7 +50,7 @@ public class ContainerConfig {
                          .map(s -> s.get(this));
         }
 
-        InstanceSupplier<?> instanceSupplier = suppliers.get(ref.getComponentType());
+        InstanceSupplier<?> instanceSupplier = getSupplier(ref);
         if (ref.isContainerType()) {
           if (ref.getContainerType() != Supplier.class) {
             return Optional.empty();
@@ -63,18 +62,22 @@ public class ContainerConfig {
     };
   }
 
-  private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
-    for (Ref dependency : suppliers.get(component).dependencies()) {
-      Class<?> componentType = dependency.getComponentType();
-      if (!suppliers.containsKey(componentType)) {
-        throw new DependencyNotFoundException(component, componentType);
+  private InstanceSupplier<?> getSupplier(Ref ref) {
+    return instanceSuppliers.get(new InstanceType(ref.getComponentType(), ref.getQualifier()));
+  }
+
+  private void checkDependencies(InstanceType component, Stack<Class<?>> visiting) {
+    for (Ref dependency : instanceSuppliers.get(component).dependencies()) {
+      InstanceType dependencyType = new InstanceType(dependency.getComponentType(), dependency.getQualifier());
+      if (!instanceSuppliers.containsKey(dependencyType)) {
+        throw new DependencyNotFoundException(component.type(), dependency.getComponentType());
       }
       if (!dependency.isContainerType()) {
-        visiting.push(component);
-        if (visiting.contains(componentType)) {
+        visiting.push(component.type());
+        if (visiting.contains(dependencyType.type())) {
           throw new CyclicDependencyFoundException(visiting);
         }
-        checkDependencies(componentType, visiting);
+        checkDependencies(dependencyType, visiting);
         visiting.pop();
       }
     }
